dcbf2f9cfc088da3a30ad79816e93ca0acf21fd6
Dave Willmer <dave.willmer@gmail.com>
2015-08-21 14:44:39 +0100

    fix whitespace snafu

--- examples/jupyter-output-area/src/index.ts
+++ examples/jupyter-output-area/src/index.ts
@@ -12,56 +12,56 @@ import BoxPanel = phosphor.widgets.BoxPanel;
 
 
 class OutputAreaWidget extends Widget {
-	private _output_view: oa.OutputView;
+  private _output_view: oa.OutputView;
 
-	constructor( model: oa.OutputModel, document: any ) {
-		super();
+  constructor( model: oa.OutputModel, document: any ) {
+    super();
 
-		this._output_view = new oa.OutputView(model, document);
-		document.querySelector('body').appendChild(this._output_view.el);
-	}
+    this._output_view = new oa.OutputView(model, document);
+    document.querySelector('body').appendChild(this._output_view.el);
+  }
 
 } // class OutputAreaWidget
 
 function fetchJSONFile(path: string) {
   return new Promise(function(resolve, reject) {
-		var httpRequest = new XMLHttpRequest();
-		httpRequest.onreadystatechange = function() {
-			if (httpRequest.readyState === 4) {
-				if (httpRequest.status === 200) {
-					resolve(JSON.parse(httpRequest.responseText));
-				} else {
-					reject(Error('httpRequest.status === ' + httpRequest.status));
-				}
-			}
-		};
-		httpRequest.open('GET', path, true);
-		httpRequest.send();
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          resolve(JSON.parse(httpRequest.responseText));
+        } else {
+          reject(Error('httpRequest.status === ' + httpRequest.status));
+        }
+      }
+    };
+    httpRequest.open('GET', path, true);
+    httpRequest.send();
   });
 }
 
 
 function main(): void {
 
-	var panel = new BoxPanel();
+  var panel = new BoxPanel();
 
-	var model = new oa.OutputModel();
-	var output_area = new OutputAreaWidget( model, document );
+  var model = new oa.OutputModel();
+  var output_area = new OutputAreaWidget( model, document );
 
-	fetchJSONFile('http://localhost:8000/data.json').then(function(messages: any[]) {
-		for (var i = 0; i < messages.length; i++) {
-			model.consumeMessage(<JSON>(messages[i]));
-		}
-	}).catch(function(err) {
-		console.error('Could not load sample data: ', err);
-	});
+  fetchJSONFile('http://localhost:8000/data.json').then(function(messages: any[]) {
+    for (var i = 0; i < messages.length; i++) {
+      model.consumeMessage(<JSON>(messages[i]));
+    }
+  }).catch(function(err) {
+    console.error('Could not load sample data: ', err);
+  });
 
-	panel.addWidget(output_area, 1);
+  panel.addWidget(output_area, 1);
 
-	panel.attach(document.getElementById('main'));
-	panel.fit();
+  panel.attach(document.getElementById('main'));
+  panel.fit();
 
-	window.onresize = () => panel.fit(); 
+  window.onresize = () => panel.fit(); 
 
 }
 
--- examples/jupyter-output-area/src/jupyter-js-output-area.d.ts
+++ examples/jupyter-output-area/src/jupyter-js-output-area.d.ts
@@ -1,16 +1,16 @@
 
 declare module 'jupyter-js-output-area' {
 
-	export class OutputModel {
-		constructor();
-		consumeMessage( msg: any ): boolean;
-	}
+  export class OutputModel {
+    constructor();
+    consumeMessage( msg: any ): boolean;
+  }
 
-	export class OutputView {
-		public el: any;
+  export class OutputView {
+    public el: any;
 
-		constructor( model: OutputModel, document: any );
+    constructor( model: OutputModel, document: any );
 
-	}
+  }
 
 }
