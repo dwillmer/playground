066f8dcff87aa8ed162933fd1965afd16ffd1a6b
Dave Willmer <dave.willmer@gmail.com>
2015-08-25 11:43:25 +0000

    Make server serve all required files

--- examples/jupyter-output-area/server.py
+++ examples/jupyter-output-area/server.py
@@ -1,3 +1,6 @@
+
+import os
+import os.path
 try:
     from http.server import SimpleHTTPRequestHandler
     import http.server as BaseHTTPServer
@@ -6,10 +9,21 @@ except ImportError:
     import BaseHTTPServer
 
 
-class CORSRequestHandler( SimpleHTTPRequestHandler ):
-    def end_headers( self ):
-        self.send_header( 'Access-Control-Allow-Origin', '*' )
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.node_dir = os.sep+'node_modules'
+        self.main_dir = os.sep.join(os.getcwd().split(os.sep)[:-2])
+        SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
 
+    def translate_path(self, path):
+        if path.startswith(self.node_dir):
+            return self.main_dir+path
+        else:
+            return SimpleHTTPRequestHandler.translate_path(self, path)
+
 if __name__ == '__main__':
-    BaseHTTPServer.test( CORSRequestHandler, BaseHTTPServer.HTTPServer )
+    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
